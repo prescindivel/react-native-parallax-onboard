948a11e7ec2f392455e52c55701c77153af0da94
Victor Martins <victor.santos@fortbrasil.com.br>
2020-02-07 16:24:22 -0300

    style: configure eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+  },
+  extends: ['airbnb', 'react-airbnb-prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-native', 'jsx-a11y', 'import', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['error', {extensions: ['.js', '.jsx']}],
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    'react/jsx-one-expression-per-line': 'off',
+    'react-native/no-color-literals': 'off',
+    'react-native/sort-styles': 'off',
+    'global-require': 'off',
+    'react-native/no-raw-text': 'off',
+  },
 };
